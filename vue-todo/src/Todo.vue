237b5ef8e547af1e814429df76b136d64442cc6d
Kei Sato <ckeisato@gmail.com>
2016-11-06 22:41:04 -0500

    Updates"

---
+++ vue-todo/src/Todo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <span v-show="!inEditMode">{{title}}</span>
+    <input v-show="inEditMode">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: "",
+      inEditMode: false
+    }
+  },
+  methods: {
+    clicked: function() {
+      this.inEditMode = true;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- vue-todo/src/TodoList.vue
+++ vue-todo/src/TodoList.vue
@@ -1,15 +1,43 @@
+
 <template>
   <div id="todolist">
     <h3>{{ title }}</h3>
+
+    <ul v-for="(todo, index) in todos">
+      <li>
+        <todo v-bind:todo="todo"></todo><button type="button" v-on:click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+    <input type="text" v-model="todoText" v-on:keyup.enter="createTodo">
+
   </div>
 </template>
 
 <script>
+import Todo from './Todo.vue';
+
 export default {
   name: 'todolist',
   data () {
     return {
-      title: 'Todo List Title'
+      title: 'To Do list',
+      todoText: '',
+      todos: []
+    }
+  },
+  components: {
+    Todo
+  },
+  methods: {
+    createTodo: function() {
+      var todoText = this.todoText.trim();
+      if (todoText) {
+        this.todos.push({ title: todoText });
+        this.todoText = '';
+      }
+    },
+    removeTodo: function(index) {
+      this.todos.splice(index, 1);
     }
   }
 }
