75bc8a396d667c71fd25320b887c6fe73cd48987
Kei Sato <ckeisato@gmail.com>
2016-11-06 21:42:49 -0500

    test

---
+++ vue-todo/src/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="todolist">
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todolist',
+  data () {
+    return {
+      title: 'Todo List Title'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- vue-todo/src/main.js
+++ vue-todo/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import TodoList from './TodoList.vue'
 
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+
+new Vue({
+	el: '#todolist',
+	render: h => h(TodoList)
 })
